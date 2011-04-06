dbff912349bfced6ff341bd82727196ee6157042
Renaud Detry <renaud.detry@uliege.be>
2011-04-06 09:42:19 +0000

    version 0.2.2
    git-svn-id: https://svn.code.sf.net/p/trsl/code/trunk@65 0ff8d33f-3b41-0410-894f-d9b890a9fedd

--- trsl/common.hpp
+++ trsl/common.hpp
@@ -16,7 +16,7 @@
  *
  * Defined as <em>major</em>.<em>minor</em>.<em>bugfix</em>.
  */
-#define TRSL_VERSION "0.2.1"
+#define TRSL_VERSION "0.2.2"
 
 /**
  * @brief Code version number.
@@ -24,7 +24,7 @@
  * Defined as 1<em>MMmmbbii</em>, where <em>MM</em> is the major release number, <em>mm</em> is the minor
  * release number, <em>bb</em> is the bug-fix release number, and <em>ii</em> is the internal release number.
  */
-#define TRSL_VERSION_NR 100020101
+#define TRSL_VERSION_NR 100020200
 
 namespace trsl {
   
