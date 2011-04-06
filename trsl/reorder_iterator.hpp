e37ca6e3e785a00c0986676e91cde0d8e655204d
Renaud Detry <renaud.detry@uliege.be>
2011-04-06 14:29:27 +0000

    Copyright
    git-svn-id: https://svn.code.sf.net/p/trsl/code/trunk@68 0ff8d33f-3b41-0410-894f-d9b890a9fedd

--- trsl/reorder_iterator.hpp
+++ trsl/reorder_iterator.hpp
@@ -1,7 +1,7 @@
 // (C) Copyright Toon Knapen    2001.
 // (C) Copyright David Abrahams 2003.
 // (C) Copyright Roland Richter 2003.
-// (C) Copyright Renaud Detry   2007-2008.
+// (C) Copyright Renaud Detry   2007-2011.
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
